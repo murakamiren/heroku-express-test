4585fa61a64322cb84040c6e7a1512e5dd24e8c1
renmurakami <autoeyy@gmail.com>
2022-04-21 17:04:31 +0900

    restore

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -21,11 +21,7 @@ route.get("/:id", async (req: Request, res: Response) => {
 	const data = await prisma.user.findUnique({
 		where: { id: parseInt(req.params.id) },
 	});
-	if (data) {
-		res.json([data]);
-	} else {
-		res.json([]);
-	}
+	res.json([data]);
 });
 
 export default route;
